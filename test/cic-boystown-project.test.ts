20de34a1571e3d2ef13944992378d2bea641ef61
aaravmat1209 <aaravmatalia129@gmail.com>
2025-05-02 13:52:33 -0700

    Initial commit

---
+++ test/cic-boystown-project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CicBoystownProject from '../lib/cic-boystown-project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cic-boystown-project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CicBoystownProject.CicBoystownProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
